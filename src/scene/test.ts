0486e44f70eaac160f76829a83807fe3a88eae88
harukaM <sphilar.infelyor@gmail.com>
2025-02-11 13:59:23 +0900

    モーダル風

--- src/scene/test.ts
+++ src/scene/test.ts
@@ -6,11 +6,24 @@ class test extends Phaser.Scene {
     }
 
     create() {
-        const testtext = this.add.text(400,200,'testtesttest', {fontSize: '60px'});
+
+        const {width, height} = this.sys.game.canvas;
+
+        const back = this.add.rectangle(width/2,height/2,width,height,0x000000,0.5);
+        back.setInteractive({
+            useHandCursor: true
+        })
+
+        const content = this.add.rectangle(width/2,height/2,600,400,0xffffff);
+        content.setInteractive({
+            userHandCursor: true
+        })
+
+        const testtext = this.add.text(400,200,'testtesttest', {fontSize: '60px', color:'000000'});
         testtext.setInteractive({
             useHandCursor: true
         })
-        testtext.on('pointerdown', () => {
+        back.on('pointerdown', () => {
             this.scene.sleep();
         })
     }
