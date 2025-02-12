8d6026b0a0f8d974891377ba751659a6228a33ec
harukaM <sphilar.infelyor@gmail.com>
2025-02-12 11:21:00 +0900

    遷移するよ

--- src/scene/test.ts
+++ src/scene/test.ts
@@ -7,6 +7,9 @@ class test extends Phaser.Scene {
 
     create() {
 
+        const pages = 3;
+        var currentpage = 1;
+
         const {width, height} = this.sys.game.canvas;
 
         const back = this.add.rectangle(width/2,height/2,width,height,0x000000,0.5);
@@ -16,18 +19,51 @@ class test extends Phaser.Scene {
 
         const content = this.add.rectangle(width/2,height/2,600,400,0xffffff);
         content.setInteractive({
+            userHandCursor: false
+        })
+
+        const prev = this.add.rectangle(380,height/2,50,50,0x000000);
+        prev.setInteractive({
+            userHandCursor:true
+        })
+
+        const next = this.add.rectangle(880,height/2,50,50,0x000000);
+        next.setInteractive({
             userHandCursor: true
         })
 
-        const testtext = this.add.text(400,200,'testtesttest', {fontSize: '60px', color:'000000'});
-        testtext.setInteractive({
-            useHandCursor: true
-        })
+        buttonvisible();
+
 
         
         back.on('pointerdown', () => {
             this.scene.sleep();
         })
+
+        prev.on('pointerdown', () => {
+          currentpage -= 1;
+          buttonvisible();  
+        })
+
+        next.on('pointerdown', () => {
+            currentpage += 1;
+            buttonvisible();
+        })
+
+
+        function buttonvisible() {
+            if(currentpage==1) {
+                prev.setVisible(false);
+                next.setVisible(true);
+            }  
+            else if(currentpage===pages) {
+                prev.setVisible(true);
+                next.setVisible(false);
+            } else {
+                prev.setVisible(true);
+                next.setVisible(true);
+            }
+        }
     }
 }
 
