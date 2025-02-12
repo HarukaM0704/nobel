609e68dfecf0a7a6cdcc056b263bf48de93d34f8
harukaM <sphilar.infelyor@gmail.com>
2025-02-12 11:43:19 +0900

    ちょっとかえた

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,12 @@
 import * as Phaser from 'phaser';
 import { Scenes } from './scene/scenes';
 
-const scale:Phaser.Types.Core.ScaleConfig = {
-  mode: Phaser.Scale.FIT,
-  autoCenter: Phaser.Scale.CENTER_BOTH,
-};
-
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,  // webGLを使うかcanvasを使うかをphaserが自動で判断してくれる
   width: window.innerWidth,
   height: window.innerHeight,
+  mode: Phaser.Scale.FIT,
+  autoCenter: Phaser.Scale.CENTER_BOTH,
   backgroundColor:'0xffa791',
   parent: 'game-app',  // #game-app内にcanvasを生成
   scene: Scenes
